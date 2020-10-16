53fffb3f30da1736c1e6b8bf7655284a31ba5aa2
Alexander-Merz <web.engineering.merz@gmail.com>
2020-10-16 16:22:30 +0200

    Extend gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
-const gulp      = require('gulp');
-const webpack   = require('webpack-stream');
+const gulp          = require('gulp');
+const webpack       = require('webpack-stream');
+const sass          = require('gulp-sass');
+const autoprefixer  = require('gulp-autoprefixer');
+sass.compiler       = require('node-sass');
 
-const { task, src, dest } = gulp;
-
-task('default', function () {
-    return src('src/index.js')
-        .pipe(webpack(require('./webpack.config.js')))
-        .pipe(dest('dist/'));
+gulp.task('scss', function () {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false,
+            flexbox: "no-2009",
+            grid: 'autoplace'
+        }))
+        .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('scss:watch', function () {
+    return gulp.watch('src/scss/*.scss', gulp.series(['scss']));
+});
+
+gulp.task('js', function () {
+    return gulp.src('src/js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('js:watch', function () {
+    return gulp.watch('src/ts/*.js', gulp.series(['js']));
+});
+
+gulp.task('dev', gulp.parallel(['scss:watch', 'js:watch']));
+
+gulp.task('build', gulp.parallel(['scss', 'js']));
