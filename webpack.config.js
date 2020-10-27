eaf69cb8887601b18a9a1301ba2502f6553a4dff
Alexander-Merz <web.engineering.merz@gmail.com>
2020-10-27 12:47:34 +0100

    Add sass loader and update extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
-const path = require('path');
-const appStructure = require('./app.structure');
+const path = require('path')
+const appStructure = require('./app.structure')
 
 module.exports = {
-    mode: 'production',
-    entry: appStructure,
-    output: {
-        path: path.resolve(__dirname, 'dist', 'js'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            exclude: /(node_modules|bower_components)/,
-            use: [{
-                    loader: 'babel-loader'
-                },
-                {
-                    loader: 'ts-loader'
-                }
-            ]
-        }]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-};
+	mode: 'production',
+	entry: appStructure,
+	output: {
+		path: path.resolve(__dirname, 'dist', 'js'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'babel-loader'
+					},
+					{
+						loader: 'ts-loader'
+					}
+				]
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', 'scss', 'css']
+	}
+}
